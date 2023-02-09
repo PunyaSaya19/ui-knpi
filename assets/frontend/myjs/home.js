449a12b03dd6e599a85c38bae387c263f7e8d309
PunyaSaya19 <rifanhdy91@gmail.com>
2023-02-09 22:48:45 +0700

    first init

---
+++ assets/frontend/myjs/home.js
@@ -0,0 +1,15 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    dots: false,
+    autoplay: true,
+    // nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        }
+    }
+})
